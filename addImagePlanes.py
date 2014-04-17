66aa11aa2ce2e59a295e573c159cc1af595a7102
Engin Arslan <hibernationtheory@gmail.com>
2014-04-17 00:08:50 -0400

    adds a method to display image planes on a rop
    adds an additional method that would display the existing image planes
    on a rop. the reasoning being, it is currently a little hard to parse
    the houdini content on a rop as it is kind of long. The long term plan
    is to give the user the optionality to enter custom image plane names in
    the same format to be mass added by the tool.

--- addImagePlanes.py
+++ addImagePlanes.py
@@ -6,6 +6,13 @@
 # you need another function that would re-construct an imageplane object from the settings on a mantra node : done
 # you need a way to display those image planes to the user to give them a choice : done
 # I need another tool, when clicked will display the existing image planes on a ROP (for informational purposes)
+# you need to provide the user with the option to define his/her image planes, maybe have them type out what they want in a specific way which you can parse:
+    # what is the infomraiton that you are looking for here?
+        # float or vector? 16 or 32? per light or not? (THATS IT!)
+        # occlusion_16_v_fel
+        # depth_32_f
+    # so your display tool can return the results like this as well!
+
 
 """
 import sys
@@ -59,11 +66,6 @@ class ImagePlane(object):
         self.vm_lightexport_scope = "*"
         self.vm_lightexport_select = "*"
 
-    def __str__(self): #?
-        return str(self.displayName)
-
-    def __repr__(self): #?
-        return "ImagePlane(variableName=%r)" %(self.vm_variable_plane)
 
     def __eq__(self, other):
         if self.__dict__ == other.__dict__:
@@ -73,6 +75,15 @@ class ImagePlane(object):
 
     def __ne__(self, other):
         return not self.__eq__(other)
+
+    #def __getattribute__(self, name):
+    #    return "ImagePlane"
+
+    def __str__(self): #?
+        return str(self.displayName)
+
+    def __repr__(self): #?
+        return "ImagePlane(variableName=%r)" %(self.vm_variable_plane)
 
     def displayName(self, givenName):
         if not isinstance(givenName, str):
@@ -115,8 +126,8 @@ class MantaImagePlaneCreator(object):
 
     def __init__(self, selectedNodes = None):
         self.imagePlaneList = ImagePlaneGlobalVariables.renderPasses
-        self.selection = hou.ui.selectFromList(choices=self.imagePlaneList)
-        self.check()
+        self.main()
+
 
     def check(self):
         messageTitle = "Error Creating Image Planes"
@@ -135,19 +146,27 @@ class MantaImagePlaneCreator(object):
             hou.ui.displayMessage("Please make sure you have 'only' Mantra ROP's selected.", title = messageTitle)
             return False
 
-        self.main()
-
 
     def main(self):
-        '''operaions to be run on the selected nodes list, when the tool is envoked'''
+        '''operations to be run on the selected nodes list, when the tool is envoked'''
+
+        self.selection = hou.ui.selectFromList(choices=self.imagePlaneList)
+        self.check()
+
         for i in hou.selectedNodes():
             self.run(i)
+            self.test(i)
 
 
     def run(self, mantraNode):
         '''runs the operations necessary when the script is called'''
         currentList = self.returnExistingImagePlanes(mantraNode)
-        self.houdiniImagePlaneMenu(self.selection, self.imagePlaneList, currentList, mantraNode)
+        self.createImagePlanesOnNode(self.selection, self.imagePlaneList, currentList, mantraNode)
+
+
+    def test(self, mantraNode):
+        #self.returnExistingImagePlanes(mantraNode)
+        self.displayImagePlanesOnNode(mantraNode)
 
 
     def returnImagePlaneAmount(self, mantraNode):
@@ -177,19 +196,53 @@ class MantaImagePlaneCreator(object):
         return existingImagePlanes
 
 
-    def createImagePlane(self, mantraNode, number):
-        """creates an image plane object from the index number of an image plane on a mantraNode"""
-        ip = ImagePlane()
-        for i in ip.__dict__.iteritems():
-            parmName = i[0]+"%s" %number
-            parm = mantraNode.parm(parmName)
-            if parm:
-                ip.__dict__[i[0]] = parm.eval()
+    def displayImagePlanesOnNode(self, mantraNode):
+        """ displays the list of image planes on a node, for informational purposes"""
+        print "\nfunc displayImagePlanesOnNode"
 
-        ip.displayName(ip.vm_variable_plane)
+        imagePlaneNameList = []
+        imagePlaneObjList = self.returnExistingImagePlanes(mantraNode)
+        for i in imagePlaneObjList:
+            print i
+            humanReadableName = self.returnImagePlaneObjectReadableName(i)
+            imagePlaneNameList.append(humanReadableName)
 
-        print "image plane %s is created" % ip.vm_variable_plane
-        return ip
+        hou.ui.selectFromList(choices=imagePlaneNameList)
+
+
+    def returnImagePlaneObjectReadableName(self, imagePlaneObj):
+        """given an image plane object, creates a human readable data representation for it"""
+        #if not isinstance(imagePlaneObj, ImagePlane):
+        #    return None
+
+        vextype_plane = imagePlaneObj.vm_vextype_plane
+        vm_quantize_plane = imagePlaneObj.vm_quantize_plane
+        vm_lightexport = imagePlaneObj.vm_lightexport
+        name = imagePlaneObj.vm_variable_plane
+
+        if vextype_plane =="vector":
+            name_vextype = "v"
+        elif vextype_plane =="float":
+            name_vextype = "f"
+        else:
+            name_vextype = "U"
+
+        if vm_quantize_plane == "half":
+            name_quantize = "16"
+        elif vm_quantize_plane =="float":
+            name_quantize = "32"
+        else:
+            name_quantize = "U"
+
+        if vm_lightexport == 1:
+            name_lightexport = "PL"
+        elif vm_lightexport == 0:
+            name_lightexport = "noPL"
+        else:
+            name_lightexport = "U"
+
+        humanReadableName = "{0}_{1}_{2}_{3}".format(name, name_vextype, name_quantize, name_lightexport)
+        return humanReadableName
 
 
     def addImagePlane(self, mantraNode, imagePlane):
@@ -205,17 +258,42 @@ class MantaImagePlaneCreator(object):
                 parm.set(i[1])
 
 
-    def houdiniImagePlaneMenu(self, selection, imagePlaneList, currentList, mantraNode):
+    def checkExistsImagePlane(self, givenList, imagePlaneList, imagePlaneObjList):
+        """checks if the given list of image plane names exist on the given image plane object list"""
 
-        selection = self.selection
         eligible = []
 
-        for i in selection:
+        for i in givenList:
             iPlane = ImagePlaneCreator(imagePlaneName = imagePlaneList[i]).imagePlane
-            if not iPlane in currentList:
+            if not iPlane in imagePlaneObjList:
                 eligible.append(iPlane)
             else:
-                print "image plane : %s is already existing on the mantra node: %s" %(iPlane, mantraNode)
+                print "image plane : %s is already existing." %(iPlane)
+                continue
+
+        return eligible
+
+
+    def createImagePlane(self, mantraNode, number):
+        """creates an image plane object from the index number of an image plane on a mantraNode"""
+        ip = ImagePlane()
+        for i in ip.__dict__.iteritems():
+            parmName = i[0]+"%s" %number
+            parm = mantraNode.parm(parmName)
+            if parm:
+                ip.__dict__[i[0]] = parm.eval()
+
+        ip.displayName(ip.vm_variable_plane)
+
+        print "image plane %s is created" % ip.vm_variable_plane
+        return ip
+
+
+    def createImagePlanesOnNode(self, selection, imagePlaneList, currentList, mantraNode):
+        """creates the given image plane names on the node, provided they don't already exist"""
+
+        selection = self.selection
+        eligible = self.checkExistsImagePlane(selection, imagePlaneList, currentList)
 
         for i in eligible:
             self.addImagePlane(mantraNode, i)
